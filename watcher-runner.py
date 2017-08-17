1a1b72e5f0e0180ca2d758f7012c98ad4e9c105a
Oleg Palij <o.palij@gmail.com>
2017-08-17 15:56:56 +0300

    pypi setup

---
+++ watcher-runner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Convenience wrapper for running watcher directly from source tree. """
+
+from fs_watcher.cli import main
+
+if __name__ == '__main__':
+    main()
